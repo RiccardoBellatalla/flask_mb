b0e1bdba4e92d53d3b3bdbca35b02f162fecad86
RiccardoBellatalla <riccardo.bellatalla13@gmail.com>
2020-03-25 19:22:10 +0100

    added 'POSTS_PER_PAGE' env variable and pagination to posts displayed

--- app/forms.py
+++ app/forms.py
@@ -50,5 +50,5 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[DataRequired, Length(min=0, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['ricky1310@hotmail.it']
+	POSTS_PER_PAGE = 3
